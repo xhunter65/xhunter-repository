4d37bfc0f06c6517d0a32ab86a03050cc42333a7
xhunter54 <rafadestructor54@gmail.com>
2025-02-05 22:45:05 -0700

    Update prebadepawershell.ps1

--- prebadepawershell.ps1
+++ prebadepawershell.ps1
@@ -3,3 +3,11 @@ $variable2=" que tal?"
 $VARiable3=200
 ${VAR iable4}=300
 New-Variable -Name Variable5 600
+
+${VAR iable4}-$VARiable3
+
+$variable1+$variable2
+
+${VAR iable4}+$VARiable3
+
+${VAR iable4}-$variable5
