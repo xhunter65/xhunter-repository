e233ddacf29f5778186a49aba61cf0c600c66d1b
xhunter54 <rafadestructor54@gmail.com>
2025-02-06 20:13:11 -0700

    actualizacion al final de powershel

--- condicionales1.ps1
+++ condicionales1.ps1
@@ -166,3 +166,25 @@ switch -Wildcard ($cadena, $cadena2)
     "Hola,*" { "[$_] coincide con [Hola,*]"; Break }
     "Hola, buenas tardes" { "[$_] coincide con [Hola, buenas tardes]" }
 }
+
+$num = 10
+
+for($i = 2; $i -lt 10; $i++)
+{
+    
+    if ($1 -eq 5) { Continue }
+    $num = $num+$i
+}
+Write-Host $num
+Write-host $i
+
+$cadena = "Hola, buenas tardes"
+$cadena2 = "Hola, buenas noches"
+
+switch -Wildcard ($cadena, $cadena2)
+{
+    "Hola, buenas*" { "[$_] coincide con [Hola, buenas*]" }
+    "Hola, bue*" { "[$_] coincide con [Hola, bue*]"; Continue }
+    "Hola,*" { "[$_] coincide con [Hola,*]" }
+    "Hola, buenas tardes" { "[$_] coincide con [Hola, buenas tardes]" }
+}
