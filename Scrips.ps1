94deb088ed9e301c9508a51ff88cff08b86a455a
xhunter54 <rafadestructor54@gmail.com>
2025-02-10 12:17:33 -0700

    Create Scrips.ps1

---
+++ Scrips.ps1
@@ -0,0 +1,14 @@
+﻿try {
+    Write-Output "Todo bien"
+}
+catch {
+    Write-Output "Algo lanzo una excepcion"
+    Write-Output $_
+}
+try {
+    Start-Something -ErrorAction Stop
+}
+catch {
+    Write-Output "Algo genero una excepcion o uso Write-Error"
+    Write-Output $_
+}
