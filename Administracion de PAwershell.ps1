158d8ad7d02041c885b76a11370f65b076f18dd8
xhunter54 <rafadestructor54@gmail.com>
2025-02-10 14:19:27 -0700

    agregar administracion de powershell y ascripts

---
+++ Administracion de PAwershell.ps1
@@ -0,0 +1,134 @@
+﻿Get-Service
+
+
+Get-Service -Name Spooler
+
+
+Get-Service -DisplayName Hora*
+
+
+Get-Service | Where-Object {$_.Status -eq "Running"}
+
+
+Get-Service | Where-Object {$_.StartType -eq "Automatic"} | Select-Object Name, StartType
+
+
+Get-Service -DependentServices Spooler
+
+
+Get-Service -RequiredServices Fax
+
+
+
+Stop-Service -Name Spooler -Confirm -PassThru
+
+
+Start-Service -Name Spooler -Confirm -PassThru
+
+
+Suspend-Service -Name stisvc -Confirm -PassThru
+
+
+Get-Service | Where-Object CanPauseAndContinue -eq True
+
+
+Suspend-Service -Name Spooler
+
+
+Restart-Service -Name WSearch -Confirm -PassThru
+
+
+Set-Service -Name dcsvc -DisplayName "Servicio de virtualización de credenciales de seguridad distribuidas"
+
+
+Set-Service -Name BITS -StartupType Automatic -Confirm -PassThru | Select-Object Name, StartType
+
+
+Set-Service -Name BITS -Description "Transfiere archivos en segundo plano mediante el uso de ancho de banda de red inactivo"
+
+
+Get-CimInstance Win32_Service -Filter 'Name = "BITS"' | Format-List Name, Description
+
+
+Set-Service -Name Spooler -Status Running -Confirm -PassThru
+
+
+Set-Service -Name stisvc -Status Paused -Confirm -PassThru
+
+
+Set-Service -Name BITS -Status Stopped -Confirm -PassThru 
+
+
+Get-Process
+
+
+Get-Process -Name Acrobat
+Get-Process -Name Search*
+Get-Process -id 13948
+
+Get-Process WINWORD -FileVersionInfo
+Get-Process WINWORD -IncludeUserName
+Get-Process WINWORD -Module
+
+Stop-Process -Name Acrobat -Confirm -PassThru
+
+Stop-Process -id 10940 -Confirm -PassThru
+
+Get-Process -Name Acrobat | Stop-Process -Confirm -PassThru
+
+Start-Process -FilePath "C:\Windows\System32\Notepad.exe"
+
+Start-Process -FilePath "cmd.exe" -ArgumentList "/c mkdir NuevaCarpeta" -WorkingDirectory "C:\Users\tutor\Desktop" -PassThru
+
+Start-Process -FilePath "notepad.exe" -WindowStyle "Maximized" -PassThru
+
+Start-Process -FilePath "C:\Users\tutor\Desktop\test.txt" -Verb print -PassThru
+
+Get-Process -Name notep*
+Wait-Process -Name notepad
+Get-Process -name notep*
+
+Get-Process -name notepad
+Wait-Process -id 11568
+Get-Process -Name notep*
+
+Get-Process -name notep*
+Get-Process -Name notepad | Wait-Process
+
+Get-LocalUser
+
+Get-LocalUser -Name Invitado | Select-Object *
+
+Get-LocalUser -SID S-1-5-21-1356686170-1997479302-1560136649-501 | Select-Object *
+
+Get-LocalGroup
+
+Get-LocalGroup -Name administradores | Select-Object *
+
+Get-LocalGroup -SID S-1-5-32-544 | Select-Object *
+
+New-LocalUser -Name "Usuario" -Description "Usuario de Prueba 1" -NoPassword
+
+New-LocalUser -Name "Usuario2" -Description "Usuario de Prueba 2" -Password (ConvertTo-SecureString -AsPlainText "12345" -Force) 
+
+Get-LocalUser -Name "Usuario"
+Remove-LocalUser -Name "Usuario"
+Get-LocalUser -Name "Usuario"
+
+Get-LocalUser -Name "Usuario2" | Remove-LocalUser
+Get-LocalUser -Name "Usuario2"
+
+New-LocalGroup -Name 'Grupo1' -Description 'Grupo de prueba 1'
+
+Add-LocalGroupMember -Group Grupo1 -Member Usuario2 -Verbose
+
+Get-LocalGroupMember Grupo1
+
+Remove-LocalGroupMember -Group Grupo1 -Member Usuario
+Remove-LocalGroupMember -Group Grupo1 -Member Usuario2
+Get-LocalGroupMember Grupo1
+
+Get-LocalGroup -Name "Grupo1"
+
+Remove-LocalGroup -Name "Grupo1"
+Get-LocalGroup -Name "Grupo1"
--- Scrips.ps1
+++ Scrips.ps1
@@ -12,3 +12,150 @@ catch {
     Write-Output "Algo genero una excepcion o uso Write-Error"
     Write-Output $_
 }
+
+$comando = [System.Data.SqlClient.SqlCommand]::New($queryString, $connection)
+
+try {
+    $comando.Connection.Open()
+    $comando.ExecuteNonQuery()
+}
+finally {
+    Write-Error "Ha habido un problema con la ejecución de la query. Cerrando la conexión"
+    $comando.Connection.Close()
+}
+
+try {
+    Start-Something -Path $path -ErrorAction Stop
+}
+catch [System.IO.DirectoryNotFoundException], [System.IO.FileNotFoundException] {
+    Write-Output "El directorio o fichero no ha sido encontrado: [$path]"
+}
+catch [System.IO.IOException] {
+    Write-Output "Error de IO con el archivo: [$path]"
+}
+
+
+
+throw "No se puede encontrar la ruta: $path"
+
+throw [System.IO.FileNotFoundException] "No se puede encontrar la ruta: $path"
+
+throw [System.IO.FileNotFoundException]::new()
+
+throw [System.IO.FileNotFoundException]::new("No se puede encontrar la ruta: $path")
+
+throw (New-Object -TypeName System.IO.FileNotFoundException)
+
+throw (New-Object -TypeName System.IO.FileNotFoundException -ArgumentList "No se puede encontrar la ruta: $path")
+
+
+
+try {
+    Write-Output $PSItem.ToString()
+}
+catch {
+    Write-Output "Error capturado: $_"
+}
+
+throw [System.Exception]::new('primero')
+throw [System.Exception]::new('segundo')
+throw [System.Exception]::new('tercero')
+
+function Backup-Registry {
+    Param(
+        [Parameter(Mandatory = $true)]
+        [string]$rutaBackup
+    )
+
+    # Crear la ruta de destino del backup si no existe
+    if (!(Test-Path -Path $rutaBackup)) {
+        New-Item -ItemType Directory -Path $rutaBackup -Force | Out-Null
+    }
+
+    # Generar un nombre único para el archivo de backup
+    $nombreArchivo = "Backup-Registry_" + (Get-Date -Format "yyyy-MM-dd_HH-mm-ss") + ".reg"
+    $rutaArchivo = Join-Path -Path $rutaBackup -ChildPath $nombreArchivo
+
+    # Realizar el backup del registro del sistema y guardarlo en el archivo de destino
+    try {
+        Write-Verbose "Realizando backup del registro del sistema en $rutaArchivo..."
+        reg export HKLM "$rutaArchivo"
+        Write-Verbose "El backup del registro del sistema se ha realizado con éxito en $rutaArchivo."
+    }
+    catch {
+        Write-Error "Se ha producido un error al realizar el backup del registro del sistema: $_"
+        return $false  # Indicar que el backup falló
+    }
+
+    return $true  # Indicar que el backup fue exitoso
+}
+
+
+# Escribir en el archivo de log
+$logDirectory = "$env:APPDATA\RegistryBackup"
+$logFile = Join-Path $logDirectory "backup-registry_log.txt"
+$logEntry = "$(Get-Date) - $env:USERNAME - Backup - $backupPath"
+
+if (!(Test-Path $logDirectory)) {
+    New-Item -ItemType Directory -Path $logDirectory | Out-Null
+}
+
+Add-Content -Path $logFile -Value $logEntry
+
+
+# Verificar si hay más de $backupCount backups en el directorio y
+# eliminar los más antiguos si es necesario
+$backupCount = 10
+$backups = Get-ChildItem $backupDirectory -Filter "*.reg" | Sort-Object LastWriteTime -Descending
+
+if ($backups.Count -gt $backupCount) {
+    $backupsToDelete = $backups[$backupCount..($backups.Count - 1)]
+    $backupsToDelete | Remove-Item -Force
+}
+
+@{
+    ModuleVersion = '1.0.0'
+    PowerShellVersion = '5.1'
+    RootModule = 'Backup-Registry.ps1'
+    Description = 'Módulo para realizar backups del registro del sistema de Windows'
+    Author = 'Alice'
+    FunctionsToExport = @('Backup-Registry')
+}
+
+ls
+
+Import-Module BackupRegistry
+
+Get-Help Backup-Registry
+
+Backup-Registry -rutaBackup 'C:\tmp\Backups\Registro\'
+
+vim .\Backup-Registry.ps1
+Import-Module BackupRegistry -Force
+Backup-Registry -rutaBackup 'C:\tmp\Backups\Registro\'
+
+ls 'C:\tmp\Backups\Registro\'
+
+# Configuración de la tarea
+$Time = New-ScheduledTaskTrigger -At 02:00 -Daily
+
+# Acción de la tarea
+$PS = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-Command `"Import-Module BackupRegistry -Force; Backup-Registry -rutaBackup 'D:\tmp\Backups\Registro' `""
+
+# Crear la tarea programada
+Register-ScheduledTask -TaskName "Ejecutar Backup del Registro del Sistema" -Trigger $Time -Action $PS
+
+ls 'C:\tmp\Backups\Registro\'
+Get-date
+
+ls 'C:\tmp\Backups\Registro\'
+Get-Date
+
+Get-ScheduledTask
+
+Unregister-ScheduledTask ‘Ejecutar Backup del Registro 
+del Sistema’
+Get-ScheduledTask
+
+
+
