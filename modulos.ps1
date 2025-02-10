2ef388a520164e90788e136ab22ddb7805859389
xhunter54 <rafadestructor54@gmail.com>
2025-02-10 11:15:47 -0700

    Create modulos.ps1

---
+++ modulos.ps1
@@ -0,0 +1,26 @@
+﻿Get-Module
+
+
+Get-Module -ListAvailable
+
+
+
+Get-Module
+
+
+Remove-Module BitsTransfer
+Get-Module
+
+Get-Command -Module BitsTransfer
+
+
+
+Get-Help BitsTransfer
+
+
+$env:PSModulePath
+
+
+Import-Module BitsTransfer
+
+Get-Module
